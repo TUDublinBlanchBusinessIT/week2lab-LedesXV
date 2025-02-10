750d4713821ecc23c211d48fe11527bf44e4a22b
LedesXV <b00164804@mytudublin.ie>
2025-02-10 17:35:50 +0000

    Completed Part A

---
+++ PartA.py
@@ -0,0 +1,17 @@
+#Part A
+#Ledes Ajuonoma B00164804
+#10/02/2025
+#A Program to Print Steps 4 to 6 to the Screen
+
+def printSteps():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
+
+def main():
+    printSteps()
+
+
+
+main()
